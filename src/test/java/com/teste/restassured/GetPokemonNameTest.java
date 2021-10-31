c2ef5e9fc728480294dcbab82a9f044acecd1275
Paulo Marques <pauloedums@gmail.com>
2021-10-31 12:29:51 -0300

    Adição de asserts

--- src/test/java/com/teste/restassured/GetPokemonNameTest.java
+++ src/test/java/com/teste/restassured/GetPokemonNameTest.java
@@ -1,6 +1,7 @@
 package com.teste.restassured;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -8,7 +9,11 @@ import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 
 import io.restassured.RestAssured;
-import static io.restassured.RestAssured.given;;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+
+import static io.restassured.RestAssured.given;
 
 @TestInstance(Lifecycle.PER_CLASS)
 public class GetPokemonNameTest {
@@ -19,12 +24,16 @@ public class GetPokemonNameTest {
 
    @Test
    public void GetPokemonNameSuccess() {
-      String pokemonName = given().get("pokemon/bulbasaur").then().statusCode(200).extract().path("name");
+      ExtractableResponse<Response> pokemon = given().get("pokemon/bulbasaur").then().statusCode(200).extract();
+      String statusCodeResponse = pokemon.response().getStatusLine();
+      String pokemonName = pokemon.path("name");
+      assertEquals("HTTP/1.1 200 OK", statusCodeResponse);
       assertEquals("bulbasaur", pokemonName);
    }
 
    @Test
    public void GetPokemonNameFailureStatusCode() {
-      given().get("pokemon/123abc").then().statusCode(404);
+      String statusCodeResponse = given().get("pokemon/123abc").then().statusCode(404).extract().asString();
+      assertEquals("Not Found", statusCodeResponse);
    }
 }
